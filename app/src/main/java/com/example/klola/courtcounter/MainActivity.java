786c5c6df69b571b9a001268d9ef50712e331d00
klolapc\klola <ariantony@gmail.com>
2017-05-05 13:52:52 +0700

    Revisi Logika Penjumlahan, Done

--- app/src/main/java/com/example/klola/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/klola/courtcounter/MainActivity.java
@@ -6,8 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    int quantity_a = 0 ;
-    int quantity_b = 0 ;
+    int quantity_a;
+    int quantity_b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,27 +18,33 @@ public class MainActivity extends AppCompatActivity {
 //fungsi untuk display Team A
 
     public void aTigaPoint (View view) {
-        display_a (quantity_a + 3);
+        quantity_a =  quantity_a + 3;
+        display_a (quantity_a);
     }
 
     public void aDuaPoint (View view) {
-        display_a (quantity_a + 2);
+        quantity_a =  quantity_a + 2;
+        display_a (quantity_a);
     }
 
     public void aFreePoint (View view) {
-        display_a (quantity_a + 1);
+        quantity_a =  quantity_a + 1;
+        display_a (quantity_a);
     }
 //fungsi untuk display Team B
     public void bTigaPoint (View view) {
-        display_b (quantity_b + 3);
+        quantity_b = quantity_b + 3;
+        display_b (quantity_b);
     }
 
     public void bDuaPoint (View view) {
-        display_b (quantity_b + 2);
+        quantity_b = quantity_b + 2;
+        display_b (quantity_b);
     }
 
     public void bFreePoint (View view) {
-        display_b (quantity_b + 1);
+        quantity_b = quantity_b + 1;
+        display_b (quantity_b);
     }
 
     public void aResetPoint (View view) {
@@ -56,6 +62,4 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.display_b);
         quantityTextView.setText("" + j);
     }
-
-
 }
